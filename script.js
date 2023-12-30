e60814cfafed9a38c17bd38930f6c9c72bd40c44
GavinEscanilla <gavinaltonescanilla@gmail.com>
2023-12-30 11:00:51 -0500

    setting up buttons

---
+++ script.js
@@ -0,0 +1,15 @@
+const button1 = document.getElementById('I');
+const button2 = document.getElementById('II');
+const button3 = document.getElementById('III');
+const button4 = document.getElementById('IV');
+const pfp =getElementById('pfp');
+
+
+ function img1() {
+getElementById("I").src="Naruto_newshot.webp";
+
+
+}
+function img2() {
+getElementById("II").src="Naruto_Part_II.webp";
+}
