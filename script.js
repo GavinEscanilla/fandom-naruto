fb17bb83a13d20bfebace4bb7c0b33191bdcb804
GavinEscanilla <gavinaltonescanilla@gmail.com>
2024-01-01 15:35:54 -0500

    fixed position of addtext and section header name

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const button1 = document.getElementById('I');
 const button2 = document.getElementById('II');
 const button3 = document.getElementById('III');
-const button4 = document.getElementById('IV');
 const pfp =getElementById('pfp');
 
 
